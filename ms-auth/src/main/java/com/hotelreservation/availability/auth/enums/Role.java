ab1995bdbca49bd0c93f69eccd9c50698183e9b8
Gabriel <luquegabrielies@gmail.com>
2026-05-30 23:45:04 +0200

    docs: add Javadoc to Role enum

--- ms-auth/src/main/java/com/hotelreservation/availability/auth/enums/Role.java
+++ ms-auth/src/main/java/com/hotelreservation/availability/auth/enums/Role.java
@@ -1,5 +1,15 @@
 package com.hotelreservation.availability.auth.enums;
 
+import com.hotelreservation.availability.auth.models.entities.User;
+
+/**
+ * Represents the security and access roles available within the system.
+ * <p>
+ * These roles are used to control authorization and define the permissions
+ * granted to a {@link User}.
+ * </p>
+ * @author Gabriel Luque
+ */
 public enum Role {
     ROLE_USER,
     ROLE_ADMIN,
