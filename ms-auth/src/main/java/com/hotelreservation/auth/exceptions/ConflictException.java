208e651ed12bf93fdeb5c5f324148bd67edca354
Gabriel <luquegabrielies@gmail.com>
2026-06-05 16:21:33 +0200

    feat: add ConflictException for state conflic errors

---
+++ ms-auth/src/main/java/com/hotelreservation/auth/exceptions/ConflictException.java
@@ -0,0 +1,8 @@
+package com.hotelreservation.auth.exceptions;
+
+/** Exception thrown when a request conflicts with the current state of the server. */
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
